1897380b5b4192f163e4121cf46658757be8e9cb
Anders Magnusson <anders14magnusson@gmail.com>
2020-06-03 01:55:51 +0200

    Add allowed origins to CORS

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 let scope = process.env.SCOPE || "user-read-private user-read-email";
+let allowedOrigins = [
+  "http://localhost:3000/SpotifyU",
+  "https://gimme0.github.io/SpotifyU",
+];
 
 let express = require("express");
 let request = require("request");
@@ -28,7 +32,24 @@ var generateRandomString = function (length) {
 
 let app = express();
 
-app.use(cors()).use(cookieParser());
+app
+  .use(
+    cors({
+      origin: function (origin, callback) {
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+          var msg =
+            "The CORS policy for this site does not " +
+            "allow access from the specified Origin.";
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+    })
+  )
+  .use(cookieParser());
 
 app.get("/login", function (req, res) {
   var state = generateRandomString(16);
