f40ad3c5056e3aca929765cfa90c647f0758dbbb
Anders Magnusson <anders14magnusson@gmail.com>
2020-06-03 02:07:12 +0200

    Add CORS wildcard

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let scope = process.env.SCOPE || "user-read-private user-read-email";
 let allowedOrigins = [
-  "http://localhost:3000/SpotifyU",
+  "http://localhost:3000",
   "https://gimme0.github.io/SpotifyU",
 ];
 
@@ -35,18 +35,7 @@ let app = express();
 app
   .use(
     cors({
-      origin: function (origin, callback) {
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-          var msg =
-            "The CORS policy for this site does not " +
-            "allow access from the specified Origin.";
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
+      origin: "*",
     })
   )
   .use(cookieParser());
